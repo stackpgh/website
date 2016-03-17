1de7a823f58c5e56b0f216351c84d2f0e0701bfe
James Turnshek <turnshek@gmail.com>
2016-03-17 15:20:02 -0400

    change running port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app     = express();
 var path    = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
@@ -11,6 +13,6 @@ app.use('/img', express.static(__dirname + '/img'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 
-app.listen(3000);
-
-console.log('running on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
